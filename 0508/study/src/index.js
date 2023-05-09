1d58352dd86d5976ec50103ebc506da15b6ea1a5
seohaneul <seo1533618@gmail.com>
2023-05-09 11:03:18 +0900

    FEAT : todo router 설정

--- 0508/study/src/index.js
+++ 0508/study/src/index.js
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import Router from './pages/router';
+import './reset.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Router />);
--- 0508/study/src/pages/router.jsx
+++ 0508/study/src/pages/router.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './main';
-import Todos from './todos';
 
 function Router() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Main />} />
-        <Route path='/todos' element={<Todos />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
 export default Router;
